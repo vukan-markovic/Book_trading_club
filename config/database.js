8d11e383962a2b194d90c51f041cb0f078db15e4
Vukan <vukan.markovic97@gmail.com>
2018-06-15 15:44:11 +0200

    b

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 // define and export module for local database url
 module.exports = {
-    url: 'mongodb://localhost:27017/BookTradingClub'
+    url: 'mongodb://Vukan:srbija1@ds159400.mlab.com:59400/booktradingclub'
 };
