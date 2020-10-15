9329428f7863b2e6afa765e9bdbf2470e8d5fe61
Vukan-Markovic <vukan.markovic97@gmail.com>
2020-10-15 16:09:59 +0200

    changed database url

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 // define and export module for mlab database url
 module.exports = {
-    url: 'mongodb://Vukan:srbija1@ds159400.mlab.com:59400/booktradingclub'
+    url: 'mongodb+srv://Vukan:srbija123@booktradingclub.bjaiy.gcp.mongodb.net/booktradingclub?retryWrites=true&w=majority'
     // local database url: 'mongodb://localhost:27017/BookTradingClub'
+    // mongodb://Vukan:srbija1@ds159400.mlab.com:59400/booktradingclub
 };
